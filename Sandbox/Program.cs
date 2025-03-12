6805c6dc1dc0a4b599cac525127a672ed7b32d38
Eduard Popescu <peduard13@hotmail.com>
2025-03-12 17:29:10 +0200

    extract delete folders method

--- Sandbox/Program.cs
+++ Sandbox/Program.cs
@@ -4,8 +4,18 @@ var folderPatterns = new[] { "bin", "obj" };
 
 foreach (var folderPattern in folderPatterns)
 {
-  var foldersToDelete = Directory.EnumerateDirectories(rootFolder, folderPattern, SearchOption.AllDirectories);
+  var foldersToDelete = Directory.EnumerateDirectories(
+    rootFolder,
+    folderPattern,
+    SearchOption.AllDirectories);
 
+  DeleteFolders(foldersToDelete);
+}
+
+return;
+
+static void DeleteFolders(IEnumerable<string> foldersToDelete)
+{
   foreach (var folder in foldersToDelete)
   {
     Console.WriteLine($"Deleting folder {folder}...");
